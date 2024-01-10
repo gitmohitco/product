0d3629141ecc2974c9264411fd6b2b48a7c2bf06
gitmohitco <152581299+gitmohitco@users.noreply.github.com>
2024-01-10 22:32:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,6 @@
-const warp = document.querySelector(".warp");
-const gparent = document.querySelector(".gparent");
-const parent = document.querySelector(".parent");
-const child = document.querySelector(".child");
-const button = document.getElementById("btn");
-// button.addEventListener('click',function(){
-//     alert('Button Clicked!!!');
-// });
-// warp.addEventListener('click',function(){
-//     alert('Warp Clicked...')
-// });
-// gparent.addEventListener('click',function(){
-//     alert('Grand Parent Clicked...')
-// });
-// parent.addEventListener('click',function(){
-//     alert('Parent Clicked...')
-// });
-// child.addEventListener('click',function(){
-//     alert('Child Clicked...')
-// });
-
-// console.log(date);
-// console.log(date.getDate());
-// console.log(date.getDay());
-// console.log(date.getFullYear());
-// console.log(date.getMonth());
-// console.log(date.getTime());
-// console.log(date.getTimezoneOffset());
-// console.log(date.getUTCDate());
-// console.log(date.getUTCHours());
-// console.log(date.getUTCFullYear());
-// console.log(date.getUTCMinutes());
 let arr = [];
 const mainDiv = document.getElementById('con');
 
-// window.onload = displayClock();
-// function displayClock(){
-//   var display = new Date().toLocaleTimeString();
-//   document.getElementById('dt').textContent = display;
-//   setTimeout(displayClock, 1000); 
-// }
 fetch("https://fakestoreapi.com/products")
 .then(function(response){
     console.log(response)
